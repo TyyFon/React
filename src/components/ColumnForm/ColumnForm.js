3adab34e0c392ac6ccf2add5807b70dc37387324
Grzegorz Wasik <wasikgrzegorz3@gmail.com>
2022-01-13 16:07:20 +0100

    Add icons and style to inputs ColumnForm

--- src/components/ColumnForm/ColumnForm.js
+++ src/components/ColumnForm/ColumnForm.js
@@ -1,18 +1,23 @@
 import styles from './ColumnForm.module.scss';
 import { useState } from 'react';
+import Button from './../Button/Button.js'
 
 const ColumnForm = props => {
-    const [value, setValue] = useState('');
+    const [title, setTitle] = useState('');
+    const [icon, setIcon] = useState('');
     const addColumn = e => {
         e.preventDefault();
-        props.action({ title: value });
-        setValue('');
+        props.action({ title: title, icon: icon });
+        setTitle('');
+        setIcon('');
+        
     };
 
 	return (
-        <form onSubmit={addColumn}>
-            <input type="text" value={value} onChange={e => setValue(e.target.value)} />
-            <button>Add column</button>
+        <form className={styles.form} onSubmit={addColumn}>
+            <span className={styles.span}>Title:</span> <input className={styles.input} type="text" value={title} onChange={e => setTitle(e.target.value)} />
+            <span className={styles.span}>Icon:</span> <input className={styles.input} type="text" value={icon} onChange={e => setIcon(e.target.value)} />
+            <Button>Add column</Button>
         </form>
 	);
 };
