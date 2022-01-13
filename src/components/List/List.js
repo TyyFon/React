8ecda1fe557d7ed57f4f6cd2c2cfa548a15e7d90
Grzegorz Wasik <wasikgrzegorz3@gmail.com>
2022-01-13 14:45:50 +0100

    Prepare adding new column with button

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -1,7 +1,31 @@
 import styles from './List.module.scss';
-import Column from '../Column/Column.js'
+import Column from '../Column/Column.js';
+import { useState } from 'react';
+import { useEffect } from 'react';
+import shortid from 'shortid';
 
 const List = () => {
+    
+    const [columns, setColumns] = useState([
+        { id: 1, title: 'Books', icon: 'book' },
+        { id: 2, title: 'Movies', icon: 'film' },
+        { id: 3, title: 'Games', icon: 'gamepad' }
+    ]);
+  
+    const handleSubmit = e => {
+        e.preventDefault();
+        setColumns([...columns, { id: shortid(), title: value }]);
+    };
+    
+    const [value, setValue] = useState(' ');
+    /*useEffect(() => {
+
+        setTimeout(() => {
+              setColumns([...columns, { id: 4, title: 'Test column'}]);
+        }, 2000);
+
+  }, []);*/
+
     return (
         <div className={styles.list}>
             <header className={styles.header}>
@@ -9,12 +33,15 @@ const List = () => {
             </header>
             <p className={styles.description}>Interesting things I want to check out</p>
             <div className={styles.columns}>
-               <Column icon="book" title="Books" />
-               <Column icon="gamepad" title="Movies" />
-               <Column icon="film" title="Games" />
+                {columns.map(columns => <Column key={columns.id} title={columns.title} icon={columns.icon} />)}
             </div>
+            <form onSubmit={handleSubmit}>
+			    <input type="text" value={value} onChange={e => setValue(e.target.value)}/>
+			    <button>Add column</button>
+            </form>
         </div>
     )
 }
+
 
 export default List;
