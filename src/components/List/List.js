db942bb708af7a9b00c0abf5136aebe447a44313
Grzegorz Wasik <wasikgrzegorz3@gmail.com>
2022-01-13 15:09:55 +0100

    Create ColumnForm component

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -1,47 +1,32 @@
 import styles from './List.module.scss';
-import Column from '../Column/Column.js';
+import Column from './../Column/Column';
+import ColumnForm from './../ColumnForm/ColumnForm';
 import { useState } from 'react';
-import { useEffect } from 'react';
 import shortid from 'shortid';
 
 const List = () => {
-    
     const [columns, setColumns] = useState([
         { id: 1, title: 'Books', icon: 'book' },
         { id: 2, title: 'Movies', icon: 'film' },
         { id: 3, title: 'Games', icon: 'gamepad' }
     ]);
-  
-    const handleSubmit = e => {
-        e.preventDefault();
-        setColumns([...columns, { id: shortid(), title: value }]);
-    };
-    
-    const [value, setValue] = useState(' ');
-    /*useEffect(() => {
 
-        setTimeout(() => {
-              setColumns([...columns, { id: 4, title: 'Test column'}]);
-        }, 2000);
+	const addColumn = newColumn => {
+		setColumns([...columns, { id: shortid(), title: newColumn.title }]);
+	};
 
-  }, []);*/
-
-    return (
-        <div className={styles.list}>
-            <header className={styles.header}>
-                <h1 className={styles.title}>Things to do<span>soon</span></h1>
-            </header>
-            <p className={styles.description}>Interesting things I want to check out</p>
-            <div className={styles.columns}>
-                {columns.map(columns => <Column key={columns.id} title={columns.title} icon={columns.icon} />)}
-            </div>
-            <form onSubmit={handleSubmit}>
-			    <input type="text" value={value} onChange={e => setValue(e.target.value)}/>
-			    <button>Add column</button>
-            </form>
-        </div>
-    )
-}
-
+	return (
+		<div className={styles.list}>
+			<header className={styles.header}>
+				<h2 className={styles.title}>Things to do<span>soon!</span></h2>
+			</header>
+			<p className={styles.description}>Interesting things I want to check out</p>
+			<section className={styles.columns}>
+				{columns.map(column => <Column key={column.id} title={column.title} icon={column.icon} />)}
+			</section>
+			<ColumnForm action={addColumn} />
+		</div>
+	);
+};
 
 export default List;
