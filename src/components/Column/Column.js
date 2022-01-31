24e919c7861fdef4ee0393ebd4c6ff4abffe2756
Grzegorz Wasik <wasikgrzegorz3@gmail.com>
2022-01-31 21:35:44 +0100

    Create Redux store and use it in List and Colum

--- src/components/Column/Column.js
+++ src/components/Column/Column.js
@@ -1,16 +1,17 @@
 import styles from './Column.module.scss';
 import CardForm from './../CardForm/CardForm.js';
 import Card from './../Card/Card.js'
+import { useSelector } from 'react-redux';
 
 
 
 const Column = props => {
-    console.log(props)
+    const cards = useSelector(state => state.cards.filter(card => card.columnId === props.id));
     
     return <article className={styles.column}>
         <h2 className={styles.title}><span className={styles.icon + ' fa fa-' + props.icon} />{props.title}</h2>
         <ul className={styles.cards}>
-            {props.cards.map(card => <Card key={card.id} title={card.title} />)}
+            {cards.map(card => <Card key={card.id} title={card.title} />)}
         </ul>
         <CardForm columnId={props.id}  action={props.action}/>
                 
