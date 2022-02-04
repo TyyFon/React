1a4e46c05724d97360ec87b0e28e706d519620ca
Grzegorz Wasik <wasikgrzegorz3@gmail.com>
2022-02-04 16:25:14 +0100

    Add searchString in initialState and handle and action for SerchForm input to overwrite state of searchString

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -1,11 +1,28 @@
 import styles from './SearchForm.module.scss';
 import TextInput from '../TextInput/TextInput';
 import Button from '../Button/Button.js';
+import { useState } from 'react';
+import { useDispatch } from 'react-redux';
+
 
 const SearchForm = () => {
+  const [currentSearch, setSearch] = useState('');
+  const dispatch = useDispatch();
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    dispatch({
+      type: 'UPDATE_SEARCHSTRING',
+      payload: {currentSearch},
+    });
+    setSearch('')
+  };
+
+
+
+
     return (
-        <form className={styles.searchForm}>
-            <TextInput placeholder="Search..." />
+        <form className={styles.searchForm} onSubmit={handleSubmit}>
+            <TextInput placeholder="Search..." value={ currentSearch } onChange={(e) => setSearch(e.target.value)}/>
             <Button>
                 <span className="fa fa-search" />
             </Button>
